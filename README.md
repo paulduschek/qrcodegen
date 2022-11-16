940ad94ba2015e32cd7a2a414af60a7a68e3b248
paulduschek <72552058+paulduschek@users.noreply.github.com>
2022-11-16 12:21:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# qrcodegen
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-ckimsy)
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img alt="QR logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
