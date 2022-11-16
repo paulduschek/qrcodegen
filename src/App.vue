66061fe332c02aa9c00ee3f46ab1e2bfb0fb75d9
paulduschek <72552058+paulduschek@users.noreply.github.com>
2022-11-16 12:24:40 +0100

    installed packages for qrcode and color kit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="QR logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png">
+    
   </div>
 </template>
 
